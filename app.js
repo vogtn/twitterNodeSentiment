29ad6155b6c613e773c71b5610e28de599b06337
vogtn <vogtn@uw.edu>
2016-08-28 16:45:49 -0700

    updating route change

--- app.js
+++ app.js
@@ -3,31 +3,33 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req,res){
-  var response = "<HEAD>" +
-  "<title>Twitter Sentiment Analysis</title>\n" +
-  "</HEAD>\n" +
-  "<BODY>\n" +
-  "<P>\n" +
-  "Welcome to the Twitter Sentitment Analysis app. " +
-  "What phrase would you like to analzye?\n" +
-  "</P>\n" +
-  "<FORM action=\"testSentiment\" method=\"get">"\n" +
-  "<P>\n" +
-  "Enter a phrase to evaluate: <INPUT type=\"text\" name=\"phrase\"<BR>\n" +
-  "</P>\n" +
-  "</FORM>\n" +
-  "</BODY>";
-  var phrase = req.query.phrase;
-  if(!phrase){
-    res.send(response);
-  }else{
-    sentiment(phrase,function(err,result){
-      response = 'sentiment(' + phrase + ')===' + result.score;
-      res.send(response);
+app.get('/',
+    function (req, res) {
+        var response = "<HEAD>" +
+          "<title>Twitter Sentiment Analysis</title>\n" +
+          "</HEAD>\n" +
+          "<BODY>\n" +
+          "<P>\n" +
+          "Welcome to the Twitter Sentiment Analysis app.  " +
+          "What phrase would you like to analzye?\n" +
+          "</P>\n" +
+          "<FORM action=\"/testSentiment\" method=\"get\">\n" +
+          "<P>\n" +
+          "Enter a phrase to evaluate: <INPUT type=\"text\" name=\"phrase\"><BR>\n" +
+          "<INPUT type=\"submit\" value=\"Send\">\n" +
+          "</P>\n" +
+          "</FORM>\n" +
+          "</BODY>";
+        var phrase = req.query.phrase;
+        if (!phrase) {
+            res.send(response);
+        } else {
+            sentiment(phrase, function (err, result) {
+                response = 'sentiment(' + phrase + ') === ' + result.score;
+                res.send(response);
+            });
+        }
     });
-  }
-});
 
 app.listen(port);
 console.log("Server listening on port" + port);
