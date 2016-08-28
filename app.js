24671b6c1074dcd9cbc656fe903bd68427779961
vogtn <vogtn@uw.edu>
2016-08-28 16:44:14 -0700

    updating body

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-var port = (3000);
+var port = 3000;
 var express = require('express');
 
 var app = express();
 
 app.get('/', function(req,res){
   var response = "<HEAD>" +
-  "<title>Twitter Sentiment Analysis"</title>
+  "<title>Twitter Sentiment Analysis</title>\n" +
+  "</HEAD>\n" +
+  "<BODY>\n" +
+  "<P>\n" +
+  "Welcome to the Twitter Sentitment Analysis app. " +
+  "What phrase would you like to analzye?\n" +
+  "</P>\n" +
+  "<FORM action=\"testSentiment\" method=\"get">"\n" +
+  "<P>\n" +
+  "Enter a phrase to evaluate: <INPUT type=\"text\" name=\"phrase\"<BR>\n" +
+  "</P>\n" +
+  "</FORM>\n" +
+  "</BODY>";
+  var phrase = req.query.phrase;
+  if(!phrase){
+    res.send(response);
+  }else{
+    sentiment(phrase,function(err,result){
+      response = 'sentiment(' + phrase + ')===' + result.score;
+      res.send(response);
+    });
+  }
 });
 
 app.listen(port);
