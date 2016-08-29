2c8c0c4b97797c623b00876a908cbf5d2f2a2e8b
vogtn <vogtn@uw.edu>
2016-08-28 18:02:05 -0700

    updating twitter logic

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var express = require('express');
 
 var app = express();
 
+var tweeter = new twitter({
+  consumer_key: '	odiVW1DzbLhd1f8ty2ihpkJw9',
+  consumer_secret: 'u4wYd5lTpMwjoaDCVNMY0d4cwUwntqsnoGwMEFuzfJ9jqjhhuU',
+  access_token_key: '717354259910496260-njBGpX5W9o5gb6seNsPh3ZV6ztWAmOW',
+  access_token_secret: 'Mw3p4ovQS4XeG2hxKiBARCR9IdcwU9xOZBmBlQtEp16uO'
+});
+
+app.get('/twitterCheck', function(req,res){
+  tweeter.verifyCredentials(function(error,data){
+    res.send("Hello, " + data.name + ". I am in your twitters");
+  });
+});
+
 app.get('/',
     function (req, res) {
         var response = "<HEAD>" +
