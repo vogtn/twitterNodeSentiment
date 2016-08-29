3e5ecda1c34ee20b09412de95a509893f5b2bd3b
vogtn <vogtn@uw.edu>
2016-08-28 18:46:51 -0700

    adding ntwitter

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var port = 3000;
 var express = require('express');
 
 var app = express();
-var twitter = require("twitter");
+var twitter = require("ntwitter");
 
 var tweeter = new twitter({
   consumer_key: '	odiVW1DzbLhd1f8ty2ihpkJw9',
   consumer_secret: 'u4wYd5lTpMwjoaDCVNMY0d4cwUwntqsnoGwMEFuzfJ9jqjhhuU',
-  access_token_key: '717354259910496260-njBGpX5W9o5gb6seNsPh3ZV6ztWAmOW',
+  access_token_key: '717354259910496260njBGpX5W9o5gb6seNsPh3ZV6ztWAmOW',
   access_token_secret: 'Mw3p4ovQS4XeG2hxKiBARCR9IdcwU9xOZBmBlQtEp16uO'
 });
 
-app.get('/twitterCheck', function(req,res){
-  tweeter.verifyCredentials(function(error,data){
-    res.send("Hello, " + data.name + ". I am in your twitters.");
-  });
+app.get('/twitterCheck', function (req, res) {
+    tweeter.verifyCredentials(function (error, data) {
+        res.send("Hello, " + data.name + ".  I am in your twitters.");
+    });
 });
 
 app.get('/',
